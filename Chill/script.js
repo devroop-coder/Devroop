4e44299784d7f5d5fa46c8003c84991a62e3b8a1
Kaushik Ranjan <kaushikranjanrajkumar@gmail.com>
2025-06-10 23:41:35 +0530

    version 1.5

--- Chill/script.js
+++ Chill/script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const downloadButton = document.querySelector('.download-app');
     if (downloadButton) {
         downloadButton.addEventListener('click', () => {
-            window.open('https://drive.google.com/drive/folders/1s2LcylLoBJfXtihykWPUw7beDZIo0PJs?usp=drive_link', '_blank');
+            window.open('https://firebasestorage.googleapis.com/v0/b/devroop-7c99b.firebasestorage.app/o/Chill%20Reloaded%2FChill%20V1.5.apk?alt=media&token=a5791f50-1399-495a-8ff8-19aa571b79c2', '_blank');
         });
     }
 }); 
